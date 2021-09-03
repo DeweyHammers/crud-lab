42d400a72e480a2f8953235292b8bf199293f468
dewey <hammersdrichard@gmail.com>
2021-09-03 10:17:48 -0700

    commit tree 246fb1c21e441b97bec1c84f9f447a23ace5fa68

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
-import Review from './Review';
+import React, { Component } from "react";
+import Review from "./Review";
 
 class Reviews extends Component {
   render() {
-    return (
-      <ul>
-        Reviews
-      </ul>
-    );
+    const renderReviews = () =>
+      this.props.reviews.map((review) => {
+        if (review.restaurantId === this.props.restaurantId) {
+          return (
+            <Review
+              key={review.id}
+              review={review}
+              removeReview={this.props.removeReview}
+            />
+          );
+        }
+      });
+
+    return <ul>{renderReviews()}</ul>;
   }
-};
+}
 
 export default Reviews;
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,17 +1,28 @@
-import React, { Component } from 'react';
-import ReviewInput from '../components/reviews/ReviewInput';
-import Reviews from '../components/reviews/Reviews';
+import React, { Component } from "react";
+import ReviewInput from "../components/reviews/ReviewInput";
+import Reviews from "../components/reviews/Reviews";
+import { connect } from "react-redux";
+import { addReview, removeReview } from "../actions/index";
 
 class ReviewsContainer extends Component {
-
   render() {
     return (
       <div>
-        <ReviewInput />
-        <Reviews />
+        <ReviewInput
+          restaurantId={this.props.restaurantId}
+          addReview={this.props.addReview}
+        />
+        <Reviews
+          reviews={this.props.reviews}
+          restaurantId={this.props.restaurantId}
+          removeReview={this.props.removeReview}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default ReviewsContainer;
+export default connect((state) => ({ reviews: state.reviews }), {
+  addReview,
+  removeReview,
+})(ReviewsContainer);
